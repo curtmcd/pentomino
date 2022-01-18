801275c1c32adb7e8f95ee48d6c962f85b8cf51e
Curt McDowell <coder@fishlet.com>
2022-01-17 17:14:32 -0800

    Pentomino packing v12

--- pent.cpp
+++ pent.cpp
@@ -9,73 +9,121 @@ using namespace std;
 
 enum dir { U, D, L, R };	// Up, down, left, right
 
-#define H 6
-#define W 10
+//#define W 10
+//#define H 6
+
+//#define W 12
+//#define H 5
+
+//#define W 15
+//#define H 4
+
+#define W 20
+#define H 3
+
+#define ALLOW_FLIPPED		1
+//#define ALLOW_FLIPPED		0
 
 vector<vector<dir>> shapes[] = {
-    {{ R, R, R, R },		// I
-     { D, D, D, D }},
-    {{ R, R, R, D },		// L
+    {
+     { R, R, R, R },		// I
+     { D, D, D, D }
+    },
+    {
+     { R, R, R, D },		// L
      { D, D, D, L },
      { D, R, R, R },
      { R, L, D, D, D },
+#if ALLOW_FLIPPED
      { D, U, R, R, R },		// L flipped
      { D, D, D, R },
      { D, L, L, L },
-     { D, D, D, R }},
-    {{ R, R, D, R },		// N
+     { D, D, D, R },
+#endif
+    },
+    {
+     { R, R, D, R },		// N
      { D, D, L, D },
      { R, D, R, R },
      { D, L, D, D },
+#if ALLOW_FLIPPED
      { R, R, L, L, D, L },	// N flipped
      { D, R, D, D },
      { R, L, D, L, L },
-     { D, D, R, D }},
-    {{ R, D, R, D },		// W
+     { D, D, R, D },
+#endif
+    },
+    {
+     { R, D, R, D },		// W
      { D, L, D, L },
      { D, R, D, R },
-     { R, L, D, L, D }},
-    {{ R, R, D, D },		// V
+     { R, L, D, L, D }
+    },
+    {
+     { R, R, D, D },		// V
      { D, D, L, L },
      { D, D, R, R },
-     { R, R, L, L, D, D }},
-    {{ D, R, R, D },		// Z
+     { R, R, L, L, D, D }
+    },
+    {
+     { D, R, R, D },		// Z
      { L, D, D, L },
+#if ALLOW_FLIPPED
      { D, L, L, D },		// Z flipped
-     { R, D, D, R }},
-    {{ D, U, R, R, D },		// U
+     { R, D, D, R },
+#endif
+    },
+    {
+     { D, U, R, R, D },		// U
      { R, L, D, D, R },
      { D, R, R, U },
-     { R, D, D, L }},
-    {{ D, R, U, R },		// P
+     { R, D, D, L }
+    },
+    {
+     { D, R, U, R },		// P
      { R, L, D, R, D },
      { R, D, L, L },
      { D, R, D, L },
+#if ALLOW_FLIPPED
      { R, R, D, L },		// P flipped
      { D, L, D, R },
      { R, L, D, R, R },
-     { R, D, L, D }},
-    {{ R, R, R, L, D },		// Y
+     { R, D, L, D },
+#endif
+    },
+    {
+     { R, R, R, L, D },		// Y
      { D, D, L, R, D },
      { D, L, R, R, R },
      { D, R, L, D, D },
+#if ALLOW_FLIPPED
      { R, D, U, R, R },		// Y flipped
      { D, L, R, D, D },
      { D, R, L, L, L },
-     { D, D, R, L, D }},
-    {{ D, L, R, R, L, D }},	// X
-    {{ D, R, R, L, D },		// R
+     { D, D, R, L, D },
+#endif
+    },
+    {
+     { D, L, R, R, L, D }	// X
+    },
+    {
+     { D, R, R, L, D },		// R
      { R, L, D, L, R, D },
      { D, L, R, R, D },
      { D, R, L, D, L },
+#if ALLOW_FLIPPED
      { D, L, L, R, D },		// R flipped
      { D, L, R, D, R },
      { D, R, L, L, D },
-     { R, D, R, L, D }},
-    {{ R, R, L, D, D },		// T
+     { R, D, R, L, D },
+#endif
+    },
+    {
+     { R, R, L, D, D },		// T
      { D, L, L, R, R, D },
      { D, D, L, R, R },
-     { D, R, R, L, L, D }}
+     { D, R, R, L, L, D }
+    }
 };
 
 int grid[H][W];
