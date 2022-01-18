97653c9d912a8b57cc23ed2e25f379b5511e40d7
Curt McDowell <coder@fishlet.com>
2022-01-17 17:14:32 -0800

    Pentomino packing v8

--- pent.cpp
+++ pent.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-enum dir { F, B, L, R };
+enum dir { F = 0, B = 1, L = 2, R = 3 };
 
 vector<dir> reorient[] = {
     { F, B, L, R },
@@ -50,26 +50,12 @@ int smallest_hole_size()
     int hole[6][10];
     memset(hole, 0, sizeof (hole));
     int next = 0;
-    int h;
     map<int, int> counts;
-
-    memset(grid, 0, sizeof (grid));
-    grid[0][1] = grid[0][2] = grid[0][3] = grid[0][8] = 99;
-    grid[1][0] = grid[1][1] = grid[1][3] = grid[1][4] = grid[1][8] = grid[1][9] = 99;
-    grid[2][1] = grid[2][2] = grid[2][6] = grid[1][6] = 99;
-    grid[3][8] = grid[3][9] = 99;
-    grid[4][4] = grid[4][5] = grid[4][6] = grid[4][7] = grid[4][8] = grid[4][9] = 99;
-    grid[5][0] = grid[5][1] = grid[5][9] = 99;
-    for (int r = 0; r < 6; r++)
-	for (int c = 0; c < 10; c++)
-	    grid[r][c] = 99 - grid[r][c];
-
-    show();
 
     for (int r = 0; r < 6; r++)
 	for (int c = 0; c < 10; c++)
 	    if (grid[r][c] == 0) {
-		h = 0;
+		int h = 0;
 		if (c > 0 && hole[r][c - 1] > 0)
 		    h = hole[r][c - 1];
 		else if (r > 0) {
@@ -88,43 +74,12 @@ int smallest_hole_size()
 	    }
 
     int m = 60;
-    for (auto i : counts)
-	if (i.second < m)
-	    m = i.second;
-    memcpy(grid, hole, sizeof (grid));
-    show();
-    cout << m << endl;
+    for (auto it : counts)
+	if (it.second < m)
+	    m = it.second;
+
     return m;
 }
-
-
-    /*
-+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-|xxxxx|     |     |     |xxxxx|xxxxx|xxxxx|xxxxx|     |xxxxx|
-|xxxxx|  1  |  1  |  1  |xxxxx|xxxxx|xxxxx|xxxxx|  2  |xxxxx|
-|xxxxx|     |     |     |xxxxx|xxxxx|xxxxx|xxxxx|     |xxxxx|
-+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-|     |     |xxxxx|     |     |xxxxx|xxxxx|xxxxx|     |     |
-| ->  |  ^  |xxxxx|  1  |  1  |xxxxx|xxxxx|xxxxx|  2  |  2  |
-|     |     |xxxxx|     |     |xxxxx|xxxxx|xxxxx|     |     |
-+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-|xxxxx|     |     |xxxxx|xxxxx|xxxxx|     |xxxxx|xxxxx|xxxxx|
-|xxxxx|  1  |  1  |xxxxx|xxxxx|xxxxx|  3  |xxxxx|xxxxx|xxxxx|
-|xxxxx|     |     |xxxxx|xxxxx|xxxxx|     |xxxxx|xxxxx|xxxxx|
-+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|     |     |
-|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|  4  |  4  |
-|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|     |     |
-+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-|xxxxx|xxxxx|xxxxx|xxxxx|     |     |     |     |     |     |
-|xxxxx|xxxxx|xxxxx|xxxxx| ->  | ->  | ->  | ->  |  4  |  4  |
-|xxxxx|xxxxx|xxxxx|xxxxx|     |     |     |     |     |     |
-+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-|     |xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|
-|  5  |xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|
-|     |xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|xxxxx|
-+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-*/
 
 bool place(int r, int c, int s, dir orient, int g_old, int g_new)
 {
@@ -156,41 +111,33 @@ bool place(int r, int c, int s, dir orient, int g_old, int g_new)
 	    return false;
 	grid[r][c] = g_new;
     }
-    // To do: optimize by returning false if a placement results in
-    // a grid with any pocket of less than 5 free cells.
+    // Optimize by denying placement if it would result in the grid
+    // having any hole of empty cells less than 5 in size that would
+    // become dead space.
+    if (smallest_hole_size() < 5)
+	return false;
     return true;
-}
-
-void try_shape(int s);
-
-void try_orient(int r, int c, int s, dir orient)
-{
-    if (place(r, c, s, orient, 0, s)) {
-	if (s == 12)
-	    show();
-	else
-	    try_shape(s + 1);
-    }
-    place(r, c, s, orient, s, 0);	// undo placement (full or partial)
 }
 
 void try_shape(int s)
 {
     for (int r = 0; r < 6; r++)
-	for (int c = 0; c < 10; c++) {
-	    if (grid[r][c] == 0) {
-		try_orient(r, c, s, F);
-		try_orient(r, c, s, B);
-		try_orient(r, c, s, L);
-		try_orient(r, c, s, R);
-	    }
-	}
+	for (int c = 0; c < 10; c++)
+	    if (grid[r][c] == 0)
+		for (dir o = F; o <= R; o = dir(o + 1)) {
+		    if (place(r, c, s, o, 0, s)) {
+			if (s == 12)
+			    show();
+			else
+			    try_shape(s + 1);
+		    }
+		    place(r, c, s, o, s, 0);  // undo placement
+		}
 }
 
 int main()
 {
     memset(grid, 0, sizeof (grid));
-    //    try_shape(1);
-    smallest_hole_size();
+    try_shape(1);
     return 0;
 }
