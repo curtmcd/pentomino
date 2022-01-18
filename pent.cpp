dbdb5c6f06b84871d087dab71777027cc30ea9dc
Curt McDowell <coder@fishlet.com>
2022-01-17 17:14:32 -0800

    Pentomino packing v15

--- pent.cpp
+++ pent.cpp
@@ -15,94 +15,111 @@
 
 using namespace std;
 
-enum dir { U, D, L, R };	// Up, down, left, right
+enum dir { U, D, L, R };
 
-#define ALLOW_FLIPPED		1
-//#define ALLOW_FLIPPED		0
+const vector<vector<dir>> draw_normal[] = {
+    {{ R, R, R, R 	},	// I
+     { D, D, D, D 	}},
+    {{ R, R, R, D 	},	// L
+     { D, D, D, L 	},
+     { D, R, R, R 	},
+     { R, L, D, D, D 	}},
+    {{ R, R, D, R 	},	// N
+     { D, D, L, D 	},
+     { R, D, R, R 	},
+     { D, L, D, D 	}},
+    {{ R, D, R, D 	},	// W
+     { D, L, D, L 	},
+     { D, R, D, R 	},
+     { R, L, D, L, D 	}},
+    {{ R, R, D, D 	},	// V
+     { D, D, L, L 	},
+     { D, D, R, R 	},
+     { R, R, L, L, D, D }},
+    {{ D, R, R, D 	},	// Z
+     { L, D, D, L 	}},
+    {{ D, U, R, R, D 	},	// U
+     { R, L, D, D, R 	},
+     { D, R, R, U 	},
+     { R, D, D, L 	}},
+    {{ D, R, U, R 	},	// P
+     { R, L, D, R, D 	},
+     { R, D, L, L 	},
+     { D, R, D, L 	}},
+    {{ R, R, R, L, D 	},	// Y
+     { D, D, L, R, D 	},
+     { D, L, R, R, R 	},
+     { D, R, L, D, D 	}},
+    {{ D, L, R, R, L, D }},	// X
+    {{ D, R, R, L, D 	},	// R
+     { R, L, D, L, R, D },
+     { D, L, R, R, D 	},
+     { D, R, L, D, L 	}},
+    {{ R, R, L, D, D 	},	// T
+     { D, L, L, R, R, D },
+     { D, D, L, R, R 	},
+     { D, R, R, L, L, D }}
+};
 
-vector<vector<dir>> shapes[] = {
-    { R, R, R, R },		// I0
-    { D, D, D, D },		// I1
-    { R, R, R, D },		// L0
-    { D, D, D, L },		// L1
-    { D, R, R, R },		// L2
-    { R, L, D, D, D },		// L3
-    { D, U, R, R, R },		// L'0		('=flipped)
-    { D, D, D, R },		// L'1
-    { D, L, L, L },		// L'2
-    { R, D, D, D },		// L'3
-    { R, R, D, R },		// N0
-    { D, D, L, D },		// N1
-    { R, D, R, R },		// N2
-    { D, L, D, D },		// N2
-    { R, R, L, L, D, L },	// N'0
-    { D, R, D, D },		// N'1
-    { R, L, D, L, L },		// N'2
-    { D, D, R, D },		// N'3
-    { R, D, R, D },		// W0
-    { D, L, D, L },		// W1
-    { D, R, D, R },		// W2
-    { R, L, D, L, D }		// W3
-    { R, R, D, D },		// V0
-    { D, D, L, L },		// V1
-    { D, D, R, R },		// V2
-    { R, R, L, L, D, D }	// V3
-    { D, R, R, D },		// Z0
-    { L, D, D, L },		// Z1
-    { D, L, L, D },		// Z'0
-    { R, D, D, R },		// Z'1
-    { D, U, R, R, D },		// U0
-    { R, L, D, D, R },		// U1
-    { D, R, R, U },		// U2
-    { R, D, D, L }		// U3
-    { D, R, U, R },		// P0
-    { R, L, D, R, D },		// P1
-    { R, D, L, L },		// P2
-    { D, R, D, L },		// P3
-    { R, R, D, L },		// P'0
-    { D, L, D, R },		// P'1
-    { R, L, D, R, R },		// P'2
-    { R, D, L, D },		// P'3
-    { R, R, R, L, D },		// Y0
-    { D, D, L, R, D },		// Y1
-    { D, L, R, R, R },		// Y2
-    { D, R, L, D, D },		// Y3
-    { R, D, U, R, R },		// Y'0
-    { D, L, R, D, D },		// Y'1
-    { D, R, L, L, L },		// Y'2
-    { D, D, R, L, D },		// Y'3
-    { D, L, R, R, L, D }	// X0
-    { D, R, R, L, D },		// R0
-    { R, L, D, L, R, D },	// R1
-    { D, L, R, R, D },		// R2
-    { D, R, L, D, L },		// R3
-    { D, L, L, R, D },		// R'0
-    { D, L, R, D, R },		// R'1
-    { D, R, L, L, D },		// R'2
-    { R, D, R, L, D },		// R'3
-    { R, R, L, D, D },		// T0
-    { D, L, L, R, R, D },	// T1
-    { D, D, L, R, R },		// T2
-    { D, R, R, L, L, D }	// T3
+const vector<vector<dir>> draw_flipped[] = {
+    {},
+    {{ D, U, R, R, R 	},	// L
+     { D, D, D, R 	},
+     { D, L, L, L 	},
+     { R, D, D, D 	}},
+    {{ R, R, L, L, D, L },	// N
+     { D, R, D, D 	},
+     { R, L, D, L, L 	},
+     { D, D, R, D 	}},
+    {},
+    {},
+    {{ D, L, L, D 	},	// Z
+     { R, D, D, R 	}},
+    {},
+    {{ R, R, D, L 	},	// P
+     { D, L, D, R 	},
+     { R, L, D, R, R 	},
+     { R, D, L, D 	}},
+    {{ R, D, U, R, R 	},	// Y
+     { D, L, R, D, D 	},
+     { D, R, L, L, L 	},
+     { D, D, R, L, D 	}},
+    {},
+    {{ D, L, L, R, D 	},	// R
+     { D, L, R, D, R 	},
+     { D, R, L, L, D 	},
+     { R, D, R, L, D 	}},
+    {}
 };
 
 struct Grid {
-    Grid(int _w, int _h) : w(_w), h(_h) {
+    Grid(int _w, int _h, bool allow_flipped) : w(_w), h(_h) {
 	for (int r = 0; r < h; r++) {
 	    vector<int> row;
+
 	    for (int c = 0; c < w; c++)
-		row.push_back(0);
+		row.push_back(-1);
+
 	    grid.push_back(row);
 	}
 
-	for (int i = 0; i < 13; i++)
-	    placed.push_back(false);
+	for (auto &p : placed)
+	    p = false;
+
+	for (int i = 0; i < 12; i++) {
+	    for (auto d : draw_normal[i])
+		shapes[i].push_back(d);
+
+	    if (allow_flipped)
+		for (auto d : draw_flipped[i])
+		    shapes[i].push_back(d);
+	}
     }
 
-    void show() {
+    void show() const {
 	for (int r = 0; r < h; r++) {
 	    for (int c = 0; c < w; c++)
-		cout << setw(3) << grid[r][c] << ' ';
+		cout << setw(3) << (grid[r][c] + 1) << ' ';
 	    cout << endl;
 	}
 
@@ -110,7 +127,7 @@ struct Grid {
     }
 
     bool place(int r, int c, int s, int o, int g_old, int g_new) {
-	vector<dir> &moves = shapes[s - 1][o];
+	const vector<dir> &moves = shapes[s][o];
 
 	assert(grid[r][c] == g_old);
 
@@ -149,7 +166,7 @@ struct Grid {
 
     void try_pos(int r, int c) {
 	// Find next empty hole on the grid
-	while (grid[r][c] != 0)
+	while (grid[r][c] >= 0)
 	    if (++c == w) {
 		c = 0;
 
@@ -161,32 +178,33 @@ struct Grid {
 	    }
 
 	// Try each remaining shape
-	for (int s = 1; s <= 12; s++)
+	for (int s = 0; s < 12; s++)
 	    if (!placed[s])
-		for (int o = 0; o < shapes[s - 1].size(); o++) {
-		    if (place(r, c, s, o, 0, s)) {
+		for (int o = 0; o < shapes[s].size(); o++) {
+		    if (place(r, c, s, o, -1, s)) {
 			placed[s] = true;
 			try_pos(r, c);
 		    }
 
 		    // undo full or partial placement
-		    place(r, c, s, o, s, 0);
+		    place(r, c, s, o, s, -1);
 		    placed[s] = false;
 		}
     }
 
     int w, h;
     vector<vector<int>> grid;
-    vector<bool> placed;
+    bool placed[12];
+    vector<vector<dir>> shapes[12];
 };
 
 void
 usage()
 {
     cerr << "Usage: pent [-w <width>] [-F]" << endl;
-    cerr << "  -w <width>    Specify grid width (height is 60 / width)" << endl;
-    cerr << "                Valid widths are 20, 15, 12 and 10" << endl;
-    cerr << "  -F            Don't let pents be flipped (12 shapes instead of 18)" << endl;
+    cerr << "  -w <width>    Specify grid width, one of 20, 15, 12 or 10," << endl;
+    cerr << "                default 10. Height is 60 divided by width." << endl;
+    cerr << "  -F            Don't allow flip (12 shapes instead of 18)" << endl;
 
     exit(1);
 }
@@ -204,7 +222,7 @@ main(int argc, char *argv[])
 	    usage();
 	    break;
 	case 'w':
-	    w = atoi(argv[optind]);
+	    w = atoi(optarg);
 	    break;
 	case 'F':
 	    allow_flipped = false;
